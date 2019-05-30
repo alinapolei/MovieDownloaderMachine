ec74502bd61aaa2b5c014c21dedff59a997ae9e4
alina <alinapolei96@gmail.com>
2019-05-30 19:38:33 +0300

    on and off

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
-
         MovieDownloaderMachine machine = new MovieDownloaderMachine();
+        String input = "";
+        Scanner in = new Scanner(System.in);
+        while(true){
+            input = in.nextLine();
+            if (input.length() > 0) {
+                machine.triggerEvent(input);
+            }
+        }
     }
 }
--- src/MovieDownloaderMachine.java
+++ src/MovieDownloaderMachine.java
@@ -5,16 +5,23 @@ public class MovieDownloaderMachine {
 
     public MovieDownloaderMachine(){
         state = new Off();
+        state.entry();
     }
 
     public void setState(MovieDownloaderState state){
+        this.state.exit();
         this.state = state;
+        this.state.entry();
     }
 
     public void triggerEvent(String input){
         switch (input){
             case "turnOn":
                 state.turnOn(this);
+                break;
+            case "turnOff":
+                state.turnOff(this);
+                break;
         }
     }
 }
